b5e5d083d44c9ddde0421190bd46a7d8fd92b607
Afshal Hassan <afshal.hassan@venturedive.com>
2022-12-07 17:13:38 +0500

    Message Component updated

---
+++ src/components/Message/Message.jsx
@@ -0,0 +1,23 @@
+import { Container } from '@mui/material';
+import React from 'react';
+import "./Message.css"
+
+function Message() {
+  return (
+    <div className='message-component-module'>
+        <Container className='message-component-container'>
+            <div className='friend-data-container'>
+
+            </div>
+            <div className='messages-data'>
+
+            </div>
+            <div className='type-messages'>
+
+</div>
+        </Container>
+    </div>
+  )
+}
+
+export default Message

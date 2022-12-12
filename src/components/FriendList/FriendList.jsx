783ca9443787760328c9051c5056bce1760668bb
Afshal Hassan <afshal.hassan@venturedive.com>
2022-12-12 14:36:47 +0500

    Friends list Api integrated

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 import "./FriendList.css";
 import Chat from "./Chat.png";
-import {Container, Typography} from "@mui/material";
-
+import { Container, Typography } from "@mui/material";
+import axios from "axios";
+import { FriendListAPI } from "../../api/APIUrl";
 
 function FriendList() {
+  const [friends, setFriends] = useState([]);
+
+  const fetchFriendsOfUser = useCallback(async () => {
+    const { data } = await axios.get(FriendListAPI());
+    setFriends(data.data);
+  }, [friends]);
+
+ 
+  useEffect(() => {
+    fetchFriendsOfUser(() => {});
+  }, []);
+
   return (
-    <div className='friend-list-module'>
-      <Container className='friend-list-container'>
-        <div className='friend-list-header'>
-        <img src={Chat} className="chat-logo" alt='ChatLogo'/>
-        <Typography className='friend-list-heading'>Messenger</Typography>
+    <div className="friend-list-module">
+      <Container className="friend-list-container">
+        <div className="friend-list-header">
+          <img src={Chat} className="chat-logo" alt="ChatLogo" />
+          <Typography className="friend-list-heading">Messenger</Typography>
         </div>
-        <div className='friend-list'>
-          <div className='friend-list-data'>
-            <img className='friend-image' alt='FriendImage' src={Chat}/>
-            <Typography className='friend-name'>Michael</Typography>
-            <div className='message-div-curve'></div>
-            <div className='message-div-curve-right'></div>
-          </div>
-          
+        <div className="friend-list">
+          {friends.map((friend) => {
+            return (
+              <div className="friend-list-data" key={friend[0].user_id}>
+                <img className="friend-image" alt="FriendImage" src={Chat} />
+                <Typography className="friend-name">{friend[0].name}</Typography>
+                <div className="message-div-curve"></div>
+                <div className="message-div-curve-right"></div>
+              </div>
+            );
+          })}
         </div>
       </Container>
-
     </div>
-  )
+  );
 }
 
-export default FriendList
+export default FriendList;

7e7c0f5e86728d0eedeb5bc254e8e1c48559abca
Afshal Hassan <afshal.hassan@venturedive.com>
2022-12-12 19:13:43 +0500

    Message Component updated

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,22 +1,30 @@
-import { Container } from '@mui/material';
+import { Button, Container } from '@mui/material';
 import React from 'react';
+import SendIcon from '@mui/icons-material/Send';
 import "./Message.css"
 
 function Message() {
   return (
     <div className='message-component-module'>
-        <Container className='message-component-container'>
-            <div className='friend-data-container'>
+      <Container className='message-component-container'>
+        <div className='friend-data-container'>
 
-            </div>
-            <div className='messages-data'>
-            <div className='messsages'></div>
-            <div className='messsages-border'></div>
-            </div>
-            <div className='type-messages'>
-            
-</div>
-        </Container>
+        </div>
+        <div className='messages-data'>
+          <div className='messsages'></div>
+          <div className='messsages-border'></div>
+        </div>
+        <div className='message-send'>
+
+
+          <div className='type-messages'>
+
+          </div>
+          <Button variant="contained" className='send-btn' endIcon={<SendIcon />}>
+            Send
+          </Button>
+        </div>
+      </Container>
     </div>
   )
 }

456de90650115a5547e91412ba5b55685c7c2e12
Afshal Hassan <afshal.hassan@venturedive.com>
2023-01-06 01:15:22 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
-
 import "./App.css";
-import { Switch,Route } from 'react-router-dom';
-
+import { Switch, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { LinearProgress } from '@mui/material';
 import { lazy, Suspense } from 'react';
 
-const FriendList = lazy( () => import("./components/FriendList/FriendList"));
-const Message = lazy( () => import("./components/Message/Message"));
-const Video = lazy( () => import("./components/Video/Video"));
+const FriendList = lazy(() => import("./components/FriendList/FriendList"));
+const Message = lazy(() => import("./components/Message/Message"));
+const Video = lazy(() => import("./components/Video/Video"));
 
 function App() {
   return (
     <div className="App1">
       <Switch>
-      
-      <Suspense fallback={<div>Loading...</div>}><Route exact path="/" component={Message}><FriendList/>
-        <Message/>
-        </Route>  
-        </Suspense> 
-       <Suspense fallback={<div>Loading...</div>}><Route exact path="/video" component={Video}><Video/></Route></Suspense>
-        </Switch>
+        <Suspense fallback={<Box sx={{ width: '100%' }}>
+          <LinearProgress />
+        </Box>}><Route exact path="/" component={Message}><FriendList />
+            <Message />
+          </Route>
+          <Route exact path="/video" component={Video}><Video /></Route></Suspense>
+      </Switch>
     </div>
   );
 }

8ac274b9c64f4837741609947223e363275a650c
Afshal Hassan <afshal.hassan@venturedive.com>
2023-02-03 20:17:11 +0500

    HTTPS issue resolved

--- src/api/APIUrl.js
+++ src/api/APIUrl.js
@@ -1,11 +1,11 @@
 export const FriendListAPI = (user) => {
-   return `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_PORT}/all-friends/of-user/${user}`;
+   return `${process.env.REACT_APP_BASE_URL}/all-friends/of-user/${user}`;
 };
 
 export const PrivateRoomKey = (firstUserEmail, secondUserEmail) => {
-   return `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_PORT}/private-room/key/${firstUserEmail}/${secondUserEmail}`
+   return `${process.env.REACT_APP_BASE_URL}/private-room/key/${firstUserEmail}/${secondUserEmail}`
 };
 
 export const GetMessagesURL = (sender,receiver) => {
-   return `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_PORT}/messages/${sender}/${receiver}`;
+   return `${process.env.REACT_APP_BASE_URL}/messages/${sender}/${receiver}`;
 }
